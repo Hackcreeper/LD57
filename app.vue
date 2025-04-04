1f10eb908a1639f9425d1d20313d36dff570b2e4
Jordan Kniest <mail@jkniest.de>
2025-04-04 19:46:12 +0200

    Initial

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <TresCanvas window-size clear-color="gray">
+    <TresPerspectiveCamera />
+    <TresMesh>
+      <TresTorusGeometry :args="[2.4, 0.1, 60, 6]" />
+      <TresMeshBasicMaterial color="black" />
+    </TresMesh>
+    <TresMesh>
+      <TresTorusGeometry :args="[1.5, 0.1, 60, 6]" />
+      <TresMeshBasicMaterial color="blue" />
+    </TresMesh>
+    <TresMesh>
+      <TresTorusGeometry :args="[.5, 0.1, 60, 6]" />
+      <TresMeshBasicMaterial color="pink" />
+    </TresMesh>
+    <TresAmbientLight :intensity="1" />
+  </TresCanvas>
+</template>
