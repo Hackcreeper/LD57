fd87e3ab5f17b50cc498b10286f93d09730358b0
Jordan Kniest <mail@jkniest.de>
2025-04-06 19:28:42 +0200

    Add a few more interactions

---
+++ composables/actions/replace.ts
@@ -0,0 +1,16 @@
+import type { CardsCollectionItem } from '@nuxt/content'
+import type { BoardCard } from '~/types/Board'
+import type { Action } from '~/types/Action'
+
+export const replace: Action = (action: CardsCollectionItem['interactions'][0]['actions'][0], baseCard: BoardCard, _interactingCart: BoardCard) => {
+  const boardStore = useBoardStore()
+  const cardStore = useCardStore()
+
+  assert(action.card !== undefined, 'Action `replace` requires the `card` property to be set!')
+
+  const card = cardStore.getCardByIdentifier(action.card)
+  assert(card !== undefined, 'Card not found!')
+
+  boardStore.removeCard(baseCard)
+  boardStore.addCard(card, baseCard.x, baseCard.z)
+}
