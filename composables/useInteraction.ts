a14add8622884efe2c94bd6114a51ecd13ffc0a4
Jordan Kniest <mail@jkniest.de>
2025-04-06 21:11:36 +0200

    Allow infinite interactions

--- composables/useInteraction.ts
+++ composables/useInteraction.ts
@@ -62,19 +62,21 @@ export const useInteraction = (draggingCard: BoardCard) => {
 
     runActions(boardCard.currentInteraction.actions, boardCard, draggingCard)
 
-    // Drop the card
-    const { x, y } = getDropCoordinates(boardCard.x, boardCard.z)
     nextTick(() => {
       // If the card has health and is now down to 0 health, remove it
       if (boardCard.currentHealth !== null && boardCard.currentHealth <= 0) {
         runActions(boardCard.card.onDeath ?? [], boardCard, draggingCard)
         boardStore.removeCard(boardCard)
+
+        // TODO: Cancel infinite interactions
       }
 
       // If the interacting card has health and is now down to 0 health, remove it
       if (draggingCard.currentHealth !== null && draggingCard.currentHealth <= 0) {
         runActions(draggingCard.card.onDeath ?? [], boardCard, draggingCard)
         boardStore.removeCard(draggingCard)
+
+        // TODO: Cancel infinite interactions
       }
 
       // If the card should be consumed, reduce the amount or remove it without triggering onDeath if amount <= 0
@@ -92,7 +94,13 @@ export const useInteraction = (draggingCard: BoardCard) => {
         }
       }
 
-      boardStore.unstackCard(draggingCard, { x, y })
+      if (!boardCard.currentInteraction?.infinite) {
+        const { x, y } = getDropCoordinates(boardCard.x, boardCard.z)
+        boardStore.unstackCard(draggingCard, { x, y })
+        return
+      }
+
+      interact(boardCard)
     })
   }
 
---
+++ stories/MiningSite.story.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useDemoCards } from './composables/useDemoCards'
+
+const { spawn } = useDemoCards()
+
+const cardStore = useCardStore()
+await cardStore.init()
+
+spawn(['worker', 'worker', 'miner', 'miner-instructor', 'asteroid-field', 'asteroid-field'])
+</script>
+
+<template>
+  <div class="relative">
+    <LevelBoard />
+  </div>
+</template>
