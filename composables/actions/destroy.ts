218d7b664f35884c313fbf6d2c7d9cad201b5168
Markus Kapp <mkapp1999@gmail.com>
2025-04-07 23:17:14 +0200

    evaluated if card is stacked

--- composables/actions/destroy.ts
+++ composables/actions/destroy.ts
@@ -5,8 +5,10 @@ import type { Action } from '~/types/Action'
 export const destroy: Action = (_action: CardsCollectionItem['interactions'][0]['actions'][0], baseCard: BoardCard, _interactingCard: BoardCard) => {
   const boardStore = useBoardStore()
 
-  const { x, y } = getDropCoordinates(baseCard.x, baseCard.z)
-  boardStore.unstackCard(baseCard.stackedCard as BoardCard, { x, y })
+  if (baseCard.stackedCard) {
+    const { x, y } = getDropCoordinates(baseCard.x, baseCard.z)
+    boardStore.unstackCard(baseCard.stackedCard, { x, y })
+  }
 
   boardStore.removeCard(baseCard)
 }
