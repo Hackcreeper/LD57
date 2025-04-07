c7dff2f1e9d3b6d34f38b95c84883d3179777371
Jordan Kniest <mail@jkniest.de>
2025-04-08 00:43:39 +0200

    Add win action

---
+++ composables/actions/win.ts
@@ -0,0 +1,7 @@
+import type { CardsCollectionItem } from '@nuxt/content'
+import type { BoardCard } from '~/types/Board'
+import type { Action } from '~/types/Action'
+
+export const win: Action = (_action: CardsCollectionItem['interactions'][0]['actions'][0], _baseCard: BoardCard, _interactingCard: BoardCard) => {
+  navigateTo('/win', { external: true })
+}
