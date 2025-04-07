ecb6f47702617c7f1cf4553d9a6529490d481ed4
Jordan Kniest <mail@jkniest.de>
2025-04-08 01:13:29 +0200

    Add tooltips and descriptions for each card

--- components/Object/Card/ObjectCard.vue
+++ components/Object/Card/ObjectCard.vue
@@ -1,18 +1,55 @@
 <script lang="ts" setup>
 import type { BoardCard } from '~/types/Board'
 
+const cardStore = useCardStore()
+
 defineProps<{
   boardCard: BoardCard
 }>()
 </script>
 
 <template>
-  <ObjectCardSingle
-    v-if="!boardCard.stackedCard && !boardCard.parentCard"
-    :board-card="boardCard"
-  />
-  <ObjectCardDeck
-    v-else-if="boardCard.stackedCard"
-    :board-card="boardCard"
-  />
+  <UPopover
+    mode="hover"
+    :open-delay="400"
+    arrow
+  >
+    <ObjectCardSingle
+      v-if="!boardCard.stackedCard && !boardCard.parentCard"
+      :board-card="boardCard"
+    />
+    <ObjectCardDeck
+      v-else-if="boardCard.stackedCard"
+      :board-card="boardCard"
+    />
+
+    <template #content>
+      <div
+        class="w-[300px] !p-4"
+      >
+        <h2
+          class="!text-lg !font-bold"
+          v-text="boardCard.card.label"
+        />
+        <p v-text="boardCard.card.description" />
+
+        <template v-if="boardCard.card.interactions?.length">
+          <br><hr><br>
+
+          <p class="!text-sm">
+            Can interact with:
+          </p>
+
+          <div class="!mt-1 !text-gray-200 !space-x-2">
+            <Icon
+              v-for="interaction in boardCard.card.interactions"
+              :key="interaction.card"
+              size="2em"
+              :name="cardStore.getCardByIdentifier(interaction.card)?.icon"
+            />
+          </div>
+        </template>
+      </div>
+    </template>
+  </UPopover>
 </template>
