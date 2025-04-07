cb3c324d228f801466c6545bf3a44d9268cd8103
Markus Kapp <mkapp1999@gmail.com>
2025-04-07 23:12:06 +0200

    only reveal events if there is something to reveal

--- composables/actions/reveal.ts
+++ composables/actions/reveal.ts
@@ -2,8 +2,12 @@ import type { CardsCollectionItem } from '@nuxt/content'
 import type { Action } from '~/types/Action'
 import type { BoardCard } from '~/types/Board'
 
-export const reveal: Action = (_action: CardsCollectionItem['interactions'][0]['actions'][0], baseCard: BoardCard, _interactingCard: BoardCard) => {
+export const reveal: Action = (_action: CardsCollectionItem['interactions'][0]['actions'][0], _interactingCard: BoardCard) => {
   const progressStore = useProgressStore()
 
-  progressStore.events.find(event => !event.revealed)!.revealed = true
+  const event = progressStore.events.find(event => !event.revealed)
+
+  if (event) {
+    event.revealed = true
+  }
 }
