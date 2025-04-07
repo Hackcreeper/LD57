96944d2c19a7f743f4c42d8d5edebdfdf5a4a3b6
Markus Kapp <mkapp1999@gmail.com>
2025-04-07 20:00:19 +0200

    started destroying asteroid field

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,7 @@ function addPercentage(identifier: string, x: number, y: number, amount: number 
   const xAbsolute = (x * container.value.clientWidth / 100) - CardWidth / 2
   const yAbsolute = (y * container.value.clientHeight / 100) - CardHeight / 2
 
-  boardStore.addCard(cardStore.getCardByIdentifier(identifier) ?? cardStore.getRandomCard(), xAbsolute, yAbsolute, true, amount)
+  boardStore.addCard(cardStore.getCardByIdentifier(identifier) ?? cardStore.getRandomCard(), xAbsolute, yAbsolute, false, amount)
 }
 
 watchOnce(container, () => {
@@ -32,7 +32,6 @@ watchOnce(container, () => {
 
   addPercentage('fuel', 35, 20, 3)
   addPercentage('money', 50, 20, 400)
-  addPercentage('asteroid-field', 65, 40)
 
   addPercentage('flux-generator', 55, 80)
 })
